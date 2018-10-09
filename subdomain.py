8af26a14f1698e61fa588277efead815e6e0dd3f
root <inr404@gmail.com>
2018-10-09 18:26:43 +0700

    mantap

--- subdomain.py
+++ subdomain.py
@@ -16,6 +16,15 @@ requests.packages.urllib3.disable_warnings()
 
 merah = "\033[01;31m{0}\033[00m"
 hijau = "\033[01;35m{0}\033[00m"
+class warna:
+	HEADER = '\033[95m'
+	OKBLUE = '\033[94m'
+	OKGREEN = '\033[92m'
+	WARNING = '\033[93m'
+	FAIL = '\033[91m'
+	ENDC = '\033[0m'
+	BOLD = '\033[1m'
+	UNDERLINE = '\033[4m'
 
 w 		= merah.format(" _   ___                  _   _     \n")
 gans 	= merah.format("| | / (_)                | | | |    \n")
@@ -45,21 +54,7 @@ for l in me:
 	sys.stdout.flush()
 	time.sleep(0.005)
 print hijau.format("                       ZeroByte.ID\n")
-
-def main():
-	domain = str(raw_input("List File: "))
-	readfile = open(domain, "r")
-	domainsFound = {}
-	domainsNotFound = {}
-	print("[+] Gaining Domain...")
-	time.sleep(1)
-	print("[+] Domains Gained...")
-	time.sleep(1)
-	for data in readfile:
-		domains = data.split('\n')
-		print "[+] Loaded :",len(domains)
-		print "[+] Gaining Subdomains..."
-	headers = {'Host': 'ctsearch.entrust.com',
+headers = {'Host': 'ctsearch.entrust.com',
 		   'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36',
 		   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 		   'Accept-Language': 'en-US,en;q=0.5',
@@ -68,8 +63,12 @@ def main():
 		   'Connection': 'close',
 		   'Upgrade-Insecure-Requests': '1',
 		   'Content-Length': '0'}
-
-	url = 'https://ctsearch.entrust.com/api/v1/certificates?fields=subjectDN&domain={}&includeExpired=true&exactMatch=false&limit=5000'.format(domains[0])
+def main():
+	domain = str(raw_input("Input Domain: "))
+	time.sleep(1)
+	print "[+] Loaded : ",domain
+	print "[+] Gaining Subdomains..."
+	url = 'https://ctsearch.entrust.com/api/v1/certificates?fields=subjectDN&domain={}&includeExpired=true&exactMatch=false&limit=5000'.format(domain)
 	response = requests.get(url, headers=headers, verify=False)
 	# print response
 	domains = []
@@ -81,7 +80,54 @@ def main():
 		for domain in match:
 			if((domain[0] not in domains) and not (re.search("^\*\.", domain[0]))):
 				domains.append(domain[0])
-	dataset_list = '\n [>] '.join(domains)
-	print Fore.GREEN,dataset_list
-
+	dataset_list = '\n'.join(domains)
+	subdomains = dataset_list.split('\n')
+	count = len(subdomains)
+	sett = str(count)
+	print "[+] Total: " + sett + " Subdomains Gained "
+	tanya = str(raw_input("[+] Test the subdomains ?(y/n): "))
+	result = []
+	if tanya == 'y':
+		time.sleep(0.5)
+		for i in subdomains:
+			try:
+				r = requests.get('http://{}'.format(i),timeout=3)
+				r.raise_for_status()
+				respon = r.status_code
+				reason = str(respon)
+				if '200' not in reason:
+					print warna.FAIL + "http://{}".format(i) + " is DOWN!" + warna.ENDC
+				else:
+					live = "http://{}".format(i) + "is UP!"
+					result.append(live)
+					print warna.OKGREEN + "http://{}".format(i) + " is UP!" + warna.ENDC
+			except requests.exceptions.ConnectionError:
+				print warna.FAIL + "http://{}".format(i) + " is DOWN! " + warna.ENDC
+			except requests.exceptions.HTTPError:
+				live_error = "http://{}".format(i) + " is UP!"
+				result.append(live_error)
+				print warna.OKGREEN + "http://{}".format(i) + " is UP!" + warna.ENDC
+	
+	else:
+		print "[+] Okay, Im Exit Baby...."
+		time.sleep(1)
+	tanya = str(raw_input("[+] Do you want to save this result?(y/n): "))
+	time.sleep(0.5)
+	if tanya == 'y':
+		print warna.WARNING + "Result will be saved as txt files." + warna.ENDC
+		nama = raw_input("Input File Name: ")
+		formatfile = ".txt"
+		print warna.WARNING + "[+] Result will be saved as ",nama+formatfile + warna.ENDC
+		for i in result:
+			sttr = str(i)
+			simpen = open(nama+formatfile, 'a')
+			simpen.write('\n'+sttr)
+			simpen.close()
+		print "[+] Job Done!"
+	if tanya == 'n':
+		time.sleep(2)
+		print warna.OKBLUE + "[+] OK! No Result will be saved,"
+		time.sleep(2)
+		print "[+] Have a Nice Day, papayyy..."
+		sys.exit(3)
 main()
